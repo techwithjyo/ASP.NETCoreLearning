213a6ebf5431d75ee0e37ba151940ebb5949e38e
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2023-06-17 22:19:13 +0530

    Adding Extension Method for Custom Middleware

--- MiddleWare Examples/CustomMiddlewares/MyCustomMiddleware.cs
+++ MiddleWare Examples/CustomMiddlewares/MyCustomMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace MiddleWare_Examples.CustomMiddlewares
@@ -12,6 +13,17 @@ namespace MiddleWare_Examples.CustomMiddlewares
             await context.Response.WriteAsync("MyCustomMiddleware - Starts \n");
             await next(context);
             await context.Response.WriteAsync("MyCustomMiddleware - Ends \n");
+        }
+    }
+
+
+    //Extra code for Extension Method
+    public static class CustomMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder app)
+        {
+            var var = app.UseMiddleware<MyCustomMiddleware>();
+            return var;
         }
     }
 }
--- MiddleWare Examples/Program.cs
+++ MiddleWare Examples/Program.cs
@@ -1,12 +1,15 @@
 using MiddleWare_Examples.CustomMiddlewares;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddTransient<MyCustomMiddleware>();
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.UseMiddleware<MyCustomMiddleware>();
+
 
 app.Use(async (context, next) =>
 {
@@ -15,6 +18,11 @@ app.Use(async (context, next) =>
     // Do logging or other work that doesn't write to the Response.
     await next(context);
 });
+
+//app.UseMiddleware<MyCustomMiddleware>();
+
+//Replacement of the above line, Just for making use of UseMy() kind of method like UseAuthentication, UseRazorPages
+app.UseMyCustomMiddleware();
 
 app.Use(async (context, next) =>
 {
