7a2005057c600f18d35ccb79240b535deabba568
jyotirmoy <jmaschatak1@gmail.com>
2024-05-23 23:17:55 +0530

    Extension method for Newtonsoft.Json

---
+++ ExtensionMethods/JsonExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class JsonExtensions
+    {
+        public static string SerializeAndIndent(this object obj)
+        {
+            var dict = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(Newtonsoft.Json.JsonConvert.SerializeObject(obj));
+
+            dict.Add("Key", Guid.NewGuid());
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(dict, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new { Name = "Jyotirmoy", age = 26 };
+            string jsonString = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(jsonString);
+
+            string newJsonString = JsonExtensions.SerializeAndIndent(person);
+
+            Console.WriteLine(newJsonString);
+            Console.ReadKey();
+
+        }
+    }
+}
